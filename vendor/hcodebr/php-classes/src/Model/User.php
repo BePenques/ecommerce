4025b8c302d8651152a9624564af83865ce1e1cf
Betiza Penques <be.penques@hotmail.com>
2018-11-20 01:56:40 -0200

    crud inconpleto

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -69,6 +69,13 @@ class User extends Model{
 			$_SESSION[User::SESSION] = null;
 		}
 
+		public static function listAll()
+		{
+
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+		}
 		
 }
 
